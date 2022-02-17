3736903beb9d39824eaadffd9fd2889f26a02cd7
Juwonoh41 <edmaster41@gmail.com>
2022-02-17 12:29:35 -0800

    connecting classes

--- models/index.js
+++ models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foriegnKey: 'category_id'
+})
 // Categories have many Products
-
+Categories.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 module.exports = {
   Product,
   Category,
