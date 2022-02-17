21a62cfd206c2a4de96da54a01844e5acd8c3031
Juwonoh41 <edmaster41@gmail.com>
2022-02-17 12:12:45 -0800

    product model

--- models/Category.js
+++ models/Category.js
@@ -7,19 +7,23 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+
     {
+      //id
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
       },
+      //category name
       category_name: {
         type: DataTypes.STRING,
         allowNull: false,
       },
     },
   },
+  
   {
     sequelize,
     timestamps: false,
